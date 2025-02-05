3b128df6e1505a4ffdcfb0ad757d2194b1e49180
matexiang <matexy2024@126.com>
2025-02-05 23:47:32 +0800

    Create ErrorCode.java

---
+++ src/main/java/com/lxy/lotterysystem/common/errorcode/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.lxy.lotterysystem.common.errorcode;
+
+import lombok.Data;
+
+@Data
+public class ErrorCode {
+
+    private final Integer code;
+
+    private final String msg;
+
+    public ErrorCode(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}
