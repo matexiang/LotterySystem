2e379cd87a98e9df61bfc160db94017ba232aad4
matexiang <matexy2024@126.com>
2025-02-07 16:20:12 +0800

    Create ControllerErrorCodeConstants.java

---
+++ src/main/java/com/lxy/lotterysystem/common/errorcode/ControllerErrorCodeConstants.java
@@ -0,0 +1,19 @@
+package com.lxy.lotterysystem.common.errorcode;
+
+public interface ControllerErrorCodeConstants {
+
+    //人员模块错误码
+    ErrorCode REGISTER_ERROR = new ErrorCode(100,"注册失败");
+
+
+    //活动模块错误码
+
+
+    //奖品模块错误码
+
+
+    //抽奖模块错误码
+
+
+
+}
