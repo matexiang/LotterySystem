a38dbb91a8b63071b1e58a88fd45562dc837b629
matexiang <matexy2024@126.com>
2025-02-14 14:54:51 +0800

    Update ServiceErrorCodeConstants.java

--- src/main/java/com/lxy/lotterysystem/common/errorcode/ServiceErrorCodeConstants.java
+++ src/main/java/com/lxy/lotterysystem/common/errorcode/ServiceErrorCodeConstants.java
@@ -36,6 +36,10 @@ public interface ServiceErrorCodeConstants {
 
 
     //活动模块错误码
+    ErrorCode CREATE_ACTIVITY_INFO_IS_EMPTY = new ErrorCode(300,"创建的活动信息为空!");
+    ErrorCode ACTIVITY_USER_ERROR = new ErrorCode(301,"创建的活动关联人员异常!");
+    ErrorCode ACTIVITY_PRIZE_ERROR = new ErrorCode(302,"创建的活动关联奖品异常!");
+    ErrorCode USER_PRIZE_AMOUNT_ERROR = new ErrorCode(303,"创建的活动关联奖品及人员数量异常!");
 
 
     //奖品模块错误码
