2e2f36d785f3af0a2a46913b3467e0df158db97a
matexiang <matexy2024@126.com>
2025-03-01 20:43:29 +0800

    Create CreatePrizeParam.java

---
+++ src/main/java/com/lxy/lotterysystem/controller/param/CreatePrizeParam.java
@@ -0,0 +1,29 @@
+package com.lxy.lotterysystem.controller.param;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.lang.NonNull;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+public class CreatePrizeParam implements Serializable {
+    /**
+     * 奖品名
+     */
+    @NotBlank(message = "奖品名不能为空!")
+    private String prizeName;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 价格
+     */
+    @NotNull(message = "奖品价格不能为空!")
+    private BigDecimal price;
+}
