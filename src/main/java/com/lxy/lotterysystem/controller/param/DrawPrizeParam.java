2231bc42a030697f1e4d3c2009088f3b32666653
matexiang <matexy2024@126.com>
2025-03-08 21:54:34 +0800

    Create DrawPrizeParam.java

---
+++ src/main/java/com/lxy/lotterysystem/controller/param/DrawPrizeParam.java
@@ -0,0 +1,43 @@
+package com.lxy.lotterysystem.controller.param;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class DrawPrizeParam {
+
+    @NotNull(message = "活动id不能为空")
+    private Long activityId;
+
+    @NotNull(message = "奖品id不能为空")
+    private Long prizeId;
+
+
+
+    @NotNull(message = "中奖时间不能为空")
+    private Date winningTime;
+
+    @NotEmpty(message = "中奖者列表不能为空")
+    @Valid
+    private List<Winner> winnerList;
+
+    @Data
+    public static  class  Winner{
+
+        @NotNull(message = "中奖者id不能为空")
+        private Long userId;
+
+        @NotBlank(message = "中奖者姓名不能为空")
+        private String username;
+
+
+    }
+
+}
